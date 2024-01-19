3284c9b37e0e1a6f262c1e1345f90caf34a0cb0b
GhadeerHamed <eng.gh.a.h93@gmail.com>
2024-01-20 02:56:09 +0300

    Implement dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,12 @@ import {HStack, Image, Text} from '@chakra-ui/react'
 import logo from './../assets/logo.webp'
 
 const NavBar = () => {
-  return (
-    <HStack>
-        <Image src={logo} boxSize='60px'/>
-        <Text> NavBar</Text>
-    </HStack>
-  )
+    return (
+        <HStack>
+            <Image src={logo} boxSize='60px'/>
+            <Text> NavBar</Text>
+        </HStack>
+    )
 }
 
 export default NavBar
