8075d283702d8d3bff46a548dcc54a30a472f419
GhadeerHamed <eng.gh.a.h93@gmail.com>
2024-01-22 02:15:41 +0300

    Add heading

--- src/App.tsx
+++ src/App.tsx
@@ -45,24 +45,26 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <GameHeading gameQuery={gameQuery} />
-        <Flex paddingLeft={2} marginBottom={5}>
-          <Box marginRight={5}>
-            <PlatformSelector
-              selectedPlatform={gameQuery.platform}
-              onSelectPlatform={(platform) =>
-                setGameQuery({ ...gameQuery, platform })
-              }
+        <Box paddingLeft={2} >
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectPlatform={(platform) =>
+                  setGameQuery({ ...gameQuery, platform })
+                }
+              />
+            </Box>
+            <SortSelector
+              selectedSortOrder={selectedSortOrder}
+              onSelectSortOrder={(sortOrder) => {
+                setSelectSortOrder(sortOrder);
+                setGameQuery({ ...gameQuery, sortOrder });
+              }}
             />
-          </Box>
-          <SortSelector
-            selectedSortOrder={selectedSortOrder}
-            onSelectSortOrder={(sortOrder) => {
-              setSelectSortOrder(sortOrder);
-              setGameQuery({ ...gameQuery, sortOrder });
-            }}
-          />
-        </Flex>
+          </Flex>
+        </Box>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
