fe5eed1efd60cae58dcc1bf5f1962c1976e8877c
GhadeerHamed <eng.gh.a.h93@gmail.com>
2024-01-24 21:39:00 +0300

    Create Game attribute component

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
 import {Platform} from "./platform";
+import {Genre} from "./genre";
+import {Publisher} from "./publisher";
 
 export interface Game {
     id: number;
     name: string;
     background_image: string;
     description_raw: string;
+    genres: Genre[];
+    publishers: Publisher[];
     parent_platforms: { platform: Platform }[];
     metacritic: number;
     rating_top: number;
