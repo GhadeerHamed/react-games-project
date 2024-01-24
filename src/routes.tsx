731f9a04ba0ae75b8c17387c63e9058e0851ba00
GhadeerHamed <eng.gh.a.h93@gmail.com>
2024-01-24 18:27:07 +0300

    Error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import {createBrowserRouter} from "react-router-dom";
 import Layout from "./pages/layout";
 import HomePage from "./pages/HomePage";
 import GameDetailsPage from "./pages/GameDetailsPage";
+import ErrorPage from "./pages/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
+        errorElement: <ErrorPage />,
         element: <Layout />,
         children: [
             {index: true, element: <HomePage />},
