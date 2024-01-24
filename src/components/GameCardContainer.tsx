aebb03242845d7c8162c49ad27c9ca029401159b
GhadeerHamed <eng.gh.a.h93@gmail.com>
2024-01-24 19:40:10 +0300

    Fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -9,7 +9,10 @@ interface Props {
 const GameCardContainer = ({ children }: Props) => {
   return (
     <>
-      <Box borderRadius={10} overflow='hidden'>
+      <Box borderRadius={10} overflow='hidden'_hover={{
+          transform: 'scale(1.03)',
+          transition: 'transform .015s ease-in'
+      }}>
         {children}
       </Box>
     </>
---
+++ src/hooks/useGame.ts
@@ -0,0 +1,13 @@
+import {useQuery} from "@tanstack/react-query";
+import {CACHE_KEY_GAMES} from "../services/constants";
+import ApiClient from "../services/api-client";
+import {Game} from "./useGames";
+
+const apiClient = new ApiClient<Game>('/games')
+
+const useGame = (slug:string) => useQuery({
+    queryKey: [CACHE_KEY_GAMES, slug],
+    queryFn: () => apiClient.get(slug)
+})
+
+export default useGame
